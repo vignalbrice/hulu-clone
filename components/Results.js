2f9743cb7071e4f6d4e03b5bfa5d47a1e5383418
Brice Vignal <vignal.brice@gmail.com>
2021-05-23 14:46:11 +0200

    hulu clone 2.0 built in nextjs and tailwindcss

---
+++ components/Results.js
@@ -0,0 +1,14 @@
+import Thumbnail from "./Thumbnail";
+import FlipMove from "react-flip-move";
+
+const Results = ({ results }) => {
+  return (
+    <FlipMove className="px-5 my-10 sm:grid md:grid-cols-3 xl:grid-cols-4 3xl:flex flex-wrap justify-center">
+      {results.map((result, index) => (
+        <Thumbnail key={result.id} result={result} />
+      ))}
+    </FlipMove>
+  );
+};
+
+export default Results;
